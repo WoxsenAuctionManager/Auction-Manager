c4c195945c0b671e23b3c3dce26fc219dd43b628
ashish veerakumar <ashishveerakumar@gmail.com>
2025-09-15 07:32:22 +0000

    Upload Failed { "error": { "code": 404, "message": "The specified bucket

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,27 +3,19 @@ import { cert, getApps, getApp, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
-if (
-  !process.env.FIREBASE_PROJECT_ID ||
-  !process.env.FIREBASE_CLIENT_EMAIL ||
-  !process.env.FIREBASE_PRIVATE_KEY
-) {
+if (!process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
   throw new Error(
-    "Firebase environment variables (FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY) are not set."
+    "Missing Firebase environment variable FIREBASE_SERVICE_ACCOUNT_JSON"
   );
 }
 
-const serviceAccount = {
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-};
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
 
 const app = getApps().length
   ? getApp()
   : initializeApp({
       credential: cert(serviceAccount),
-      storageBucket: "studio-2256766213-a72a9.appspot.com",
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     });
 
 export const adminDb = getFirestore(app);
