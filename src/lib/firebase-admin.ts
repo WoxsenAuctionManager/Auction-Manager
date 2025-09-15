b4cc9d33af6715e425ea6e76e7f476fc5e3413d9
ashish veerakumar <ashishveerakumar@gmail.com>
2025-09-15 06:56:17 +0000

    Try fixing this error: `Runtime Error: SyntaxError: Expected property na

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,15 +1,25 @@
 import * as admin from 'firebase-admin';
 
-if (!process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
-  throw new Error('FIREBASE_SERVICE_ACCOUNT_JSON is not set');
+if (
+  !process.env.FIREBASE_PROJECT_ID ||
+  !process.env.FIREBASE_CLIENT_EMAIL ||
+  !processenv.FIREBASE_PRIVATE_KEY
+) {
+  throw new Error(
+    'Firebase environment variables (FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY) are not set.'
+  );
 }
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+};
 
 if (!admin.apps.length) {
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
-    storageBucket: `${serviceAccount.project_id}.appspot.com`,
+    storageBucket: `${serviceAccount.projectId}.appspot.com`,
   });
 }
 
