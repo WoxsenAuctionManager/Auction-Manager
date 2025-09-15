8b3de1bbf61381760db8f755da42c13f48f04d92
ashish veerakumar <ashishveerakumar@gmail.com>
2025-09-15 07:33:08 +0000

    Try fixing this error: `Runtime Error: SyntaxError: Expected property na

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,19 +3,27 @@ import { cert, getApps, getApp, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import { getStorage } from "firebase-admin/storage";
 
-if (!process.env.FIREBASE_SERVICE_ACCOUNT_JSON) {
+if (
+  !process.env.FIREBASE_PROJECT_ID ||
+  !process.env.FIREBASE_CLIENT_EMAIL ||
+  !process.env.FIREBASE_PRIVATE_KEY
+) {
   throw new Error(
-    "Missing Firebase environment variable FIREBASE_SERVICE_ACCOUNT_JSON"
+    "Firebase environment variables (FIREBASE_PROJECT_ID, FIREBASE_CLIENT_EMAIL, FIREBASE_PRIVATE_KEY) are not set."
   );
 }
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_JSON);
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+};
 
 const app = getApps().length
   ? getApp()
   : initializeApp({
       credential: cert(serviceAccount),
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      storageBucket: `${serviceAccount.projectId}.appspot.com`,
     });
 
 export const adminDb = getFirestore(app);
